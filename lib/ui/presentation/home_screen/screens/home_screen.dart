9f84790d8ecfb52c94ecfc0a094d1b6a99516bd5
ali_hamza_laptop <alibakdash3@gmail.com>
2025-12-03 09:55:48 +0300

    final-edits-2

--- lib/ui/presentation/home_screen/screens/home_screen.dart
+++ lib/ui/presentation/home_screen/screens/home_screen.dart
@@ -113,7 +113,7 @@ class _HomeScreenState extends State<HomeScreenPage>
     return const Scaffold(
       appBar: HeaderWidget(),
       backgroundColor: lightGrey,
-      extendBody: true, 
+      extendBody: true,
       // bottomNavigationBar: CustomBottomNavbar(
       //   currentIndex: _selectedIndex,
       //   onItemTapped: _onItemTapped,
