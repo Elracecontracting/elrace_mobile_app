2a0f6dcfd56332b32911bc6fb504e9f189c7e4fd
loay-alkhayat <lloayalkhayat20011@gmail.com>
2026-04-10 12:11:47 +0400

    fixed Media

--- lib/ui/presentation/media/screens/media_list_screen.dart
+++ lib/ui/presentation/media/screens/media_list_screen.dart
@@ -219,7 +219,7 @@ class _MediaListScreenState extends State<MediaListScreen> {
                 )
               else
                 SliverPadding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.w),
+                  padding: EdgeInsets.only(left: 16.w, right: 16.w, bottom: 40.h),
                   sliver: SliverToBoxAdapter(
                     child: Column(
                       children: [
@@ -830,7 +830,7 @@ class _MediaListScreenState extends State<MediaListScreen> {
                         itemBuilder: (context, index) {
                           final content = photos[index];
                           return Image.network(
-                            _safeImageUrl(content.previewUrl),
+                            _safeImageUrl(content.displayImageUrl),
                             headers: _imageHeaders,
                             fit: BoxFit.contain,
                             loadingBuilder: (context, child, loadingProgress) {
@@ -846,7 +846,7 @@ class _MediaListScreenState extends State<MediaListScreen> {
                             errorBuilder: (context, error, stackTrace) {
                               _logPhotoLoadError(
                                 source: 'preview',
-                                rawUrl: content.previewUrl,
+                                rawUrl: content.displayImageUrl,
                                 error: error,
                               );
                               return Container(
